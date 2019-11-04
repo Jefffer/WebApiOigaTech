3dc5d100b0b683d28b2bfea16f0137ccec10c4e9
Jefffer <jefre123@hotmail.com>
2019-11-04 11:45:44 -0500

    Agregar archivos de proyecto.

---
+++ WebApiOigaTech/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiOigaTech.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebApiOigaTech/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiOigaTech.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}

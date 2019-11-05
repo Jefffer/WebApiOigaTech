7855b8e84988b4c02634dd19e96ec928538648c0
Jefffer <jefre123@hotmail.com>
2019-11-05 11:52:17 -0500

    Calculate salaries service finished.

--- BusinessLogic/Services/EmployeeService.cs
+++ BusinessLogic/Services/EmployeeService.cs
@@ -9,16 +9,19 @@ namespace BusinessLogic.Services
 {
     public class EmployeeService : IEmployeeService
     {
+        protected int hours = 120;
+        protected int months = 12;
+
         public decimal CalculateAnnualHourlySalary(decimal hourlySalary)
         {
-
-            return 0;
+            decimal salary = hours*hourlySalary*months;
+            return salary;
         }
 
         public decimal CalculateAnnualMonthlySalary(decimal monthlySalary)
         {
-
-            return 0;
+            decimal salary = monthlySalary * months;
+            return salary;
         }
     }
 }
